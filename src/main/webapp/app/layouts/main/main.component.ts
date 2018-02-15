e45511e5cb1f409ffa63287dacf9c8fcf80a0f40
Patresss <patresss@wp.pl>
2018-02-15 17:17:16 +0100

    Added blur in plan

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRouteSnapshot, NavigationEnd } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRouteSnapshot, NavigationEnd, Router} from '@angular/router';
 
-import { JhiLanguageHelper } from '../../shared';
+import {JhiLanguageHelper} from '../../shared';
 
 @Component({
     selector: 'jhi-main',
@@ -9,10 +9,12 @@ import { JhiLanguageHelper } from '../../shared';
 })
 export class JhiMainComponent implements OnInit {
 
-    constructor(
-        private jhiLanguageHelper: JhiLanguageHelper,
-        private router: Router
-    ) {}
+    fullPage = false;
+    fullPageRegexp = [new RegExp('^\\/plan.*')];
+
+    constructor(private jhiLanguageHelper: JhiLanguageHelper,
+                private router: Router) {
+    }
 
     private getPageTitle(routeSnapshot: ActivatedRouteSnapshot) {
         let title: string = (routeSnapshot.data && routeSnapshot.data['pageTitle']) ? routeSnapshot.data['pageTitle'] : 'timetableApp';
@@ -22,10 +24,22 @@ export class JhiMainComponent implements OnInit {
         return title;
     }
 
+    private updateFullPageInfo(event: NavigationEnd) {
+        this.fullPage = false;
+        for (let regexp of this.fullPageRegexp) {
+            if (regexp.test(event.url)) {
+                this.fullPage = true;
+            } else {
+                break;
+            }
+        }
+    }
+
     ngOnInit() {
         this.router.events.subscribe((event) => {
             if (event instanceof NavigationEnd) {
                 this.jhiLanguageHelper.updateTitle(this.getPageTitle(this.router.routerState.snapshot.root));
+                this.updateFullPageInfo(event);
             }
         });
     }
