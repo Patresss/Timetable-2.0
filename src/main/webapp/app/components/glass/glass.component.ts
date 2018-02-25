0154383385f66630f7ce4cc972f529092975c9b2
Patresss <patresss@wp.pl>
2018-02-25 20:10:17 +0100

    Add glass navbar

--- src/main/webapp/app/components/glass/glass.component.ts
+++ src/main/webapp/app/components/glass/glass.component.ts
@@ -3,12 +3,12 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'glass',
   templateUrl: './glass.component.html',
-  styleUrls: ['./glass.component.scss'],
+  styleUrls: ['./glass.component.scss', '../../layouts/navbar/navbar.scss'],
 
 })
 export class GlassComponent {
   title = 'glass';
 
     @Input()
-    contentClass: string;
+    contentClass: string = 'default-glass-content';
 }
