508c0be37583d16b37228a87e06e63cf7d66d95f
Patresss <38ksm43rk>
2018-02-25 09:46:14 +0100

    Add glass component

---
+++ src/main/webapp/app/components/glass/glass.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'glass',
+  templateUrl: './glass.component.html',
+  styleUrls: ['./glass.component.scss']
+})
+export class GlassComponent {
+  title = 'glass';
+}
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -1,22 +1,23 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
-import { TimetableSharedModule } from '../shared';
+import {TimetableSharedModule} from '../shared';
 
-import { HOME_ROUTE, HomeComponent } from './';
+import {HOME_ROUTE, HomeComponent} from './';
+import {GlassComponent} from "../components/glass/glass.component";
 
 @NgModule({
-    imports: [
-        TimetableSharedModule,
-        RouterModule.forRoot([ HOME_ROUTE ], { useHash: true })
-    ],
-    declarations: [
-        HomeComponent,
-    ],
-    entryComponents: [
-    ],
-    providers: [
-    ],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  imports: [
+    TimetableSharedModule,
+    RouterModule.forRoot([HOME_ROUTE], {useHash: true})
+  ],
+  declarations: [
+    HomeComponent,
+    GlassComponent,
+  ],
+  entryComponents: [],
+  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class TimetableHomeModule {}
+export class TimetableHomeModule {
+}
