87d82b7d53bc827279ef5f1937bdbabae72abeab
Patresss <patresss@wp.pl>
2018-03-18 16:00:21 +0100

    Upgrade plan popover

--- src/main/webapp/app/plan/timetable-element/timetable-popover.component.ts
+++ src/main/webapp/app/plan/timetable-element/timetable-popover.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input} from '@angular/core';
+import {Component, HostListener, Input} from '@angular/core';
 import {EventType, Timetable} from '../../entities/timetable';
 
 @Component({
     selector: 'jhi-timetable-popover',
-    templateUrl: './timetable-popover.html'
+    templateUrl: './timetable-popover.html',
+    styleUrls: ['./timetable-popover.component.scss'],
 })
 export class TimetableElementComponent {
 
@@ -17,6 +18,26 @@ export class TimetableElementComponent {
 
     @Input()
     canModifyTimetable: boolean;
+
+    lastPopoverRef: any;
+
+    @HostListener('document:click', ['$event'])
+    clickOutside(event) {
+        // If there's a last element-reference AND the click-event target is outside this element
+        if (this.lastPopoverRef && !this.lastPopoverRef._elementRef.nativeElement.contains(event.target)) {
+            this.lastPopoverRef.close();
+            this.lastPopoverRef = null;
+        }
+    }
+
+    setCurrentPopoverOpen(popReference) {
+        // If there's a last element-reference AND the new reference is different
+        if (this.lastPopoverRef && this.lastPopoverRef !== popReference) {
+            this.lastPopoverRef.close();
+        }
+        // Registering new popover ref
+        this.lastPopoverRef = popReference;
+    }
 
     goToEdit() {
         console.log('goToEdit')
